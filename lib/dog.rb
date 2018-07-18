595bedc06e133fbde15a560a8607c656526f5c65
Taylor Williams <taylor@thewilliams.net>
2018-07-18 21:55:28 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,8 @@
 class Dog
   attr_accessor :name, :breed
+  attr_reader @id
 
-  def initialize(name:, breed:)
+  def initialize(id = nil, name:, breed:)
     @name = name
     @breed = breed
   end
