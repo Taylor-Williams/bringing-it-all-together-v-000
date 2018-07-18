e0b3957662f9418869e39fe00ba2ff43601fcd3f
Taylor Williams <taylor@thewilliams.net>
2018-07-18 22:10:53 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -19,6 +19,9 @@ class Dog
   def save
     DB[:conn].execute("INSERT INTO dogs(name, breed) VALUES (?, ?)", @name, @breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
-    self
+  end
+
+  def self.create
+
   end
 end
