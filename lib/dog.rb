909654ddb3b3dc2351f581adb7d771d83e2189c3
Taylor Williams <taylor@thewilliams.net>
2018-07-18 22:35:42 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,4 +43,8 @@ class Dog
   def self.new_from_db(row)
     self.create(id:row[0], name:row[1], breed:row[2])
   end
+
+  def self.find_by_name(name)
+    new_from_db(DB[:conn].execute("SELECT * FROM dogs WHERE dogs.name = ? LIMIT 1", name).first)
+  end
 end
